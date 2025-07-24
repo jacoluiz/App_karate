e0fc4b90ab65a4c41749f74499a1c2c24b7ea1d4
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-07-24 17:00:33 -0300

    Segurança

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "br.com.shubudo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.3"
+        versionCode = 21
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
